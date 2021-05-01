e105c3a7fd94209e3ab2c344a41c43a1c2ee3e08
kpfefer <15963543+kpfefer@users.noreply.github.com>
2021-05-01 11:21:53 +0200

    Update api.sh

--- sol/api.sh
+++ sol/api.sh
@@ -46,7 +46,6 @@ args=(
   --enable-rpc-transaction-history
   --limit-ledger-size 50000000
   --health-check-slot-distance 500
-  --cuda
   --rpc-port 8899
   --private-rpc
   --expected-genesis-hash "$EXPECTED_GENESIS_HASH"
@@ -61,8 +60,8 @@ args=(
 
 # Note: can get into a bad state that requires actually fetching a new snapshot. One such error that indicates this:
 # "...processing for bank 0 must succeed: FailedToLoadEntries(InvalidShredData(Custom(\"could not reconstruct entries\")))"
-if [[ -d /data/sol/ledger ]]; then
-  args+=(--no-snapshot-fetch)
-fi
+#if [[ -d /data/sol/ledger ]]; then
+#  args+=(--no-snapshot-fetch)
+#fi
 
 exec solana-validator "${args[@]}"
